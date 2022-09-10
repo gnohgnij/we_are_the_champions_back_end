24593edd79c2351b8e9d03449426824755dc3a00
Chong Jing Hong <congjinghong@gmail.com>
2022-09-10 23:51:17 +0800

    changed port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ connectDB();
 app.use("/api/team", require("./routes/team"));
 app.use("/api/match", require("./routes/match"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`Server is listening on port 3000`);
+  console.log(`Server is listening on port ${port}`);
 });
