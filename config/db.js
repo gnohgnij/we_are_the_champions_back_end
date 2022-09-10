598b37899e9485d9a0ad68a200b198838c9d1b39
Chong Jing Hong <congjinghong@gmail.com>
2022-09-10 23:46:44 +0800

    changed env variables

--- config/db.js
+++ config/db.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://user:dbpassword@cluster0.apmmype.mongodb.net/?retryWrites=true&w=majority",
-      () => {
-        console.log(`Connected to MongoDB`);
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, () => {
+      console.log(`Connected to MongoDB`);
+    });
   } catch (err) {
     console.error(err.message);
     process.exit(1);
